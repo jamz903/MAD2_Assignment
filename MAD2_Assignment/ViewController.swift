a7c45923cc94e6b5a392273b4d7802424011a83e
Jamie <s10194918@connect.np.edu.sg>
2021-01-07 14:04:37 +0800

    Initial Commit

---
+++ MAD2_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAD2_Assignment
+//
+//  Created by MAD2_P01 on 7/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
