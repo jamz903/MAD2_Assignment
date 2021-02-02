5488be6c963eef62f09128e3541f316a38366670
Justin Ng <s10195649@connect.np.edu.sg>
2021-02-02 14:26:49 +0800

    Added sign up validation

--- MAD2_Assignment/SignUpViewController.swift
+++ MAD2_Assignment/SignUpViewController.swift
@@ -46,47 +46,53 @@ class SignUpViewController: UIViewController{
         
         //check that the 2 datafields are not empty
         if SignUpID.text != "" && SignUpPW.text != "" {
-            //make id entered variabel into the text in the datafield
+            //make id entered variable into the text in the datafield
             if let idEntered = SignUpID.text?.lowercased() {
-                //made pw entered variable into the text of the textfield
-                if let pwEntered = SignUpPW.text {
-                    var profilesList: [Student] = [Student]()
-                    
-                    //create reference for the firebase database
-                    ref.child("Profiles").observeSingleEvent(of: .value, with: { [self] (snapshot) in
-                        //set the values of the push data as a dictionary
-                        let value = snapshot.value as! NSDictionary
+                // Checks that user has entered in a valid Student ID format (e.g. s12345678)
+                if idEntered[idEntered.startIndex] == "s" && Int(idEntered.suffix(8)) != nil {
+                    //made pw entered variable into the text of the textfield
+                    if let pwEntered = SignUpPW.text {
+                        var profilesList: [Student] = [Student]()
                         
-                        //get all the names
-                        //add the keys from the database into an array
-                        let Keys : NSArray = (value.allKeys as! [String]) as NSArray
-                        
-                        //add new member into variable before pushing it to the database
-                        for i in 0..<(Keys.count){
-                            let guy = value[Keys[i]] as! NSDictionary
-                            let student = Student(Name: Keys[i] as! String, PW: guy["PW"] as! String, Log: guy["Log"] as! String)
-                            profilesList.append(student)
-                        }
-                        
-                        //variable for later to check if the user has been created ebfore
-                        var userExists: Bool = false
-                        for i in profilesList {
-                            if i.name == idEntered { // Student ID entered is already a registered account
-                                print("Student ID already registered")
-                                userExists = true
-                                errorText.text = "Student ID already registered"
-                                break
+                        //create reference for the firebase database
+                        ref.child("Profiles").observeSingleEvent(of: .value, with: { [self] (snapshot) in
+                            //set the values of the push data as a dictionary
+                            let value = snapshot.value as! NSDictionary
+                            
+                            //get all the names
+                            //add the keys from the database into an array
+                            let Keys : NSArray = (value.allKeys as! [String]) as NSArray
+                            
+                            //add new member into variable before pushing it to the database
+                            for i in 0..<(Keys.count){
+                                let guy = value[Keys[i]] as! NSDictionary
+                                let student = Student(Name: Keys[i] as! String, PW: guy["PW"] as! String, Log: guy["Log"] as! String)
+                                profilesList.append(student)
+                            }
+                            
+                            //variable for later to check if the user has been created ebfore
+                            var userExists: Bool = false
+                            for i in profilesList {
+                                if i.name == idEntered { // Student ID entered is already a registered account
+                                    print("Student ID already registered")
+                                    userExists = true
+                                    errorText.text = "Student ID already registered"
+                                    break
+                                }
+                            }
+                            if !userExists {//push the value to the database
+                                let details = ["Log": "false", //by default
+                                               "PW": pwEntered] as [String : Any]
+                                self.ref.child("Profiles").child(idEntered).setValue(details)
+                                
+                                dismiss(animated: true, completion: nil)
                             }
                         }
-                        if !userExists {//push the value to the database
-                            let details = ["Log": "true", //by default
-                                           "PW": pwEntered] as [String : Any]
-                            self.ref.child("Profiles").child(idEntered).setValue(details)
-                            
-                            dismiss(animated: true, completion: nil)
-                        }
-                    }
-                )}
+                    )}
+                }
+                else {
+                    errorText.text = "Invalid Student ID"
+                }
             }
         }
     }
